3fa989bb3e4688ade3624afc890b72d51ebdba49
Quan Hoang <quanhoangminh94@gmail.com>
2017-04-16 18:07:59 +0700

    - Adjust OfferDetails page - Add Filter bar with basic fields
    Signed-off-by: Quan Hoang <quanhoangminh94@gmail.com>

--- classic/src/view/ads/OfferDetails.js
+++ classic/src/view/ads/OfferDetails.js
@@ -12,20 +12,23 @@ Ext.define('Admin.view.ads.OfferDetails', {
         'Ext.ux.layout.ResponsiveColumn'
     ],
 
-    layout: 'responsivecolumn',
+    layout: {
+        type: 'responsivecolumn',
+        align: 'center'
+    },
 
     items: [
         {
             xtype: 'container',
 
             // Use 50% of container when viewport is big enough, 100% otherwise
-            userCls: 'big-30 small-100',
+            userCls: 'big-20 small-100',
 
             layout: {
                 type: 'vbox',
                 align: 'center'
             },
-            
+
             items: [{
                 xtype: 'displayfield',
                 name: 'thumb',
@@ -39,121 +42,160 @@ Ext.define('Admin.view.ads.OfferDetails', {
             }]
         },
         {
-            xtype: 'container',
-
-            userCls: 'big-40 small-100',
-
-            defaults: {
-                anchor: '100%',
-                labelWidth: 150
+            xtype: 'form',
+            layout: {
+                type: 'responsivecolumn',
+                align: 'center'
             },
-            defaultType: 'displayfield',
+            userCls: 'big-60 small-100',
+            items: [
+                {
+                    xtype: 'container',
 
-            items: [{
-                fieldLabel: 'Status',
-                name: 'status',
-                renderer: function (value, field) {
-                    if (value == 'STOPPED') {
-                        return '<i class="fa fa-circle text-orange"></i> ' + value;
-                    } else {
-                        return '<i class="fa fa-circle text-green"></i> ' + value;
-                    }
+                    userCls: 'big-50 small-100',
+
+                    layout: {
+                        type: 'vbox',
+                        align: 'left'
+                    },
+                    defaults: {
+                        anchor: '100%',
+                        labelWidth: 150
+                    },
+                    defaultType: 'displayfield',
+
+                    items: [{
+                        fieldLabel: 'Status',
+                        name: 'status',
+                        renderer: function (value, field) {
+                            if (value == 'STOPPED') {
+                                return '<i class="fa fa-circle text-orange"></i> ' + value;
+                            } else {
+                                return '<i class="fa fa-circle text-green"></i> ' + value;
+                            }
+                        }
+                    }, {
+                        fieldLabel: 'Payout',
+                        name: 'price',
+                        fieldCls: 'price',
+                        renderer: function (value, field) {
+                            return Ext.util.Format.currency(value, '₫', 0, true);
+                        }
+                    }, {
+                        fieldLabel: 'Platform',
+                        name: 'platform',
+                        renderer: function (value, field) {
+                            if (value == 'Android') {
+                                return '<i class="fa fa-android"></i> ' + value;
+                            } else if (value == 'iOS') {
+                                return '<i class="fa fa-apple"></i> ' + value;
+                            } else {
+                                return '<i class="fa fa-windows"></i> ' + value;
+                            }
+                        }
+                    }, {
+                        fieldLabel: 'Region',
+                        name: 'region',
+                        renderer: function (value, field) {
+                            return '<i class="fa fa-globe"></i> ' + value;
+                        }
+                    }, {
+                        fieldLabel: 'CAP',
+                        name: 'cap'
+                    },]
+                }, {
+                    xtype: 'container',
+
+                    userCls: 'big-50 small-100',
+
+                    layout: {
+                        type: 'vbox',
+                        align: 'left'
+                    },
+                   
+                    defaults: {
+                        anchor: '100%',
+                        labelWidth: 150
+                    },
+                    defaultType: 'displayfield',
+                    items: [
+                        {
+                            fieldLabel: 'Type',
+                            name: 'type',
+                            fieldCls: 'type-incentive'
+                        }, {
+                            fieldLabel: 'Start Time (UTC)',
+                            name: 'start_date'
+                        }, {
+                            fieldLabel: 'End Time (UTC)',
+                            name: 'end_date'
+                        }, {
+
+                            fieldLabel: '<span style="font-weight: bold;">Requirement</span>',
+                            name: 'requirement',
+                            renderer: function(value, field) {
+                                return  '<p>Install and Open app.</p>'+
+                                '<p>No cheat, No fake, No duplicate, No Spam, No Bot traffic.</p>'+
+                                '<p>CR<4%, RR>=30%.</p>';
+                            }
+                               
+                        
+                        }, {
+                            fieldLabel: 'Description',
+                            name: 'description'
+                        }
+                    ]
                 }
-            }, {
-                fieldLabel: 'Payout',
-                name: 'price',
-                fieldCls: 'price',
-                renderer: function (value, field) {
-                    return Ext.util.Format.currency(value, '₫', 0, true);
-                }
-            }, {
-                fieldLabel: 'Platform',
-                name: 'platform',
-                renderer: function (value, field) {
-                    if (value == 'Android') {
-                        return '<i class="fa fa-android"></i> ' + value;
-                    } else if (value == 'iOS') {
-                        return '<i class="fa fa-apple"></i> ' + value;
-                    } else {
-                        return '<i class="fa fa-windows"></i> ' + value;
-                    }
-                }
-            }, {
-                fieldLabel: 'Region',
-                name: 'region',
-                renderer: function (value, field) {
-                    return '<i class="fa fa-globe"></i> ' + value;
-                }
-            }, {
-                fieldLabel: 'CAP',
-                name: 'cap'
-            }, {
-                fieldLabel: 'Type',
-                name: 'type',
-                fieldCls: 'type-incentive'
-            }, {
-                fieldLabel: 'Start Time (UTC)',
-                name: 'start_date'
-            }, {
-                fieldLabel: 'End Time (UTC)',
-                name: 'end_date'
-            }, {
-                fieldLabel: 'Requirement',
-                name: 'requirement'
-            }, {
-                fieldLabel: 'Description',
-                name: 'description'
-            }]
+            ]
         },
-            {
-    xtype: 'gridpanel',
-    userCls: 'big-30 small-100',
-    store: {
-        type: 'similarOffers'
-    },
-    stateful: true,
-    width: '30%',
-    height: '50%',
-    title: 'Similar Campaign',
-       layout: {
+
+        {
+            xtype: 'gridpanel',
+            userCls: 'big-20 small-100',
+            store: {
+                type: 'similarOffers'
+            },
+            stateful: true,
+            title: 'Similar Campaign',
+            layout: {
                 type: 'vbox',
                 align: 'center'
             },
-            
-    headerBorders: false,
-    viewConfig: {
-        enableTextSelection: true
-    },
-    columns : {
-            items: [
-                {
-                menuDisabled: true,
-                width: '33%',
-                sortable : true,
-                
-                renderer: function(val) {
-                    return '<img style="width: 30px; height: auto;" class="app-image" src="resources/images/apps/' + val + '" />';
-                },
-                dataIndex: 'imgUrl'
+
+            headerBorders: false,
+            viewConfig: {
+                enableTextSelection: true
             },
-             {
-                 text: 'Payout',
-                 width: '33%',
-                 dataIndex: 'payout'
-             },
-             {
-                 text: 'Platform',
-                 width: '33%',
-                 dataIndex: 'platform'
-             }
-            ]
-    },
-    
+            columns: {
+                items: [
+                    {
+                        menuDisabled: true,
+                        width: '33%',
+                        sortable: true,
+
+                        renderer: function (val) {
+                            return '<img style="width: 30px; height: auto;" class="app-image" src="resources/images/apps/' + val + '" />';
+                        },
+                        dataIndex: 'imgUrl'
+                    },
+                    {
+                        text: 'Payout',
+                        width: '33%',
+                        dataIndex: 'payout'
+                    },
+                    {
+                        text: 'Platform',
+                        width: '33%',
+                        dataIndex: 'platform'
+                    }
+                ]
+            },
+
         }
     ],
 
     buttons: [{
         text: 'Request Tracking Link'
     }]
-    
+
 });
--- classic/src/view/ads/OfferList.js
+++ classic/src/view/ads/OfferList.js
@@ -1,149 +1,241 @@
+/**
+ * Demonstrates usage of an vbox layout.
+ */
 Ext.define('Admin.view.ads.OfferList', {
-    extend: 'Ext.grid.Panel',
-    xtype: 'offer-list',
-    controller: 'offer-list',
-
+    extend: 'Ext.panel.Panel',
     requires: [
+        'Ext.layout.container.VBox',
         'Ext.toolbar.Paging',
         'Ext.ux.PreviewPlugin',
-        'Ext.grid.column.Action'
+        'Ext.form.Panel',
+        'Ext.grid.column.Action',
+        'Ext.ux.layout.ResponsiveColumn',
+        'Ext.form.field.ComboBox'
     ],
 
-    title: 'Mine',
+    xtype: 'offer-list',
+
 
     layout: {
         type: 'vbox',
+        pack: 'start',
         align: 'stretch'
     },
 
-    autoLoad: true,
-    disableSelection: true,
-    loadMask: true,
+    bodyPadding: 10,
 
-    actions: {
-        detail: {
-            text: 'Details',
-            glyph: 'xf256@FontAwesome',
-            tooltip: 'Details'
-        }
+    defaults: {
+        frame: false,
+        bodyPadding: 10
     },
 
-    store: {
-        autoLoad: true,
-        sortOnLoad: true,
-        fields: [
-            'id', 'name', 'thumb', 'platform', 'type', 'region', 'status',
-            {name: 'total_click', type: 'int'}, {name: 'total_install', type: 'int'}, 
-            {name: 'total_cr', type: 'int'}, {name: 'total_earning', type: 'int'},
-            'description'
-        ],
-        idProperty: 'id',
-        proxy: {
-            type: 'ajax',
-            url : 'resources/data/topics.json',
-            reader: {
-                type: 'json',
-                rootProperty: 'topics',
-                totalProperty: 'totalCount'
+    items: [
+        {
+            xtype: 'form',
+            layout: 'responsivecolumn',
+            margin: '0 0 10 0',
+            border: false,
+            title: 'My Offers',
+            items: [
+                {
+                    xtype: 'textfield',
+                    userCls: 'big-20 small-100',
+                    name: 'offername',
+                    fieldLabel: 'Offer Name',
+                    value: 'Offer Name'
+                },
+                {
+                    xtype: 'combobox',
+                    userCls: 'big-20 small-100',
+                    reference: 'states',
+                    publishes: 'value',
+                    fieldLabel: 'Select Offer',
+                    displayField: 'state',
+
+                    minChars: 0,
+                    queryMode: 'local',
+                    typeAhead: true
+                }
+                ,
+                {
+                   xtype: 'combobox',
+                   userCls: 'big-20 small-100',
+                    reference: 'states',
+                    publishes: 'value',
+                    fieldLabel: 'Select Status',
+                    displayField: 'state',
+
+                    minChars: 0,
+                    queryMode: 'local',
+                    typeAhead: true
+                }
+                ,
+                {
+                    xtype: 'combobox',
+                    userCls: 'big-20 small-100',
+                    reference: 'states',
+                    publishes: 'value',
+                    fieldLabel: 'Select Platform',
+                    displayField: 'state',
+
+                    minChars: 0,
+                    queryMode: 'local',
+                    typeAhead: true
+                },
+                {
+                    xtype: 'combobox',
+                    userCls: 'big-20 small-100',
+                    reference: 'states',
+                    publishes: 'value',
+                    fieldLabel: 'Select Region',
+                    displayField: 'state',
+
+                    minChars: 0,
+                    queryMode: 'local',
+                    typeAhead: true
+                }
+            ]
+        },
+        {
+            controller: 'offer-list',
+            xtype: 'gridpanel',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            autoLoad: true,
+            disableSelection: true,
+            loadMask: true,
+            actions: {
+                detail: {
+                    text: 'Details',
+                    glyph: 'xf256@FontAwesome',
+                    tooltip: 'Details'
+                }
+            },
+            store: {
+                autoLoad: true,
+                sortOnLoad: true,
+                fields: [
+                    'id', 'name', 'thumb', 'platform', 'type', 'region', 'status',
+                    { name: 'total_click', type: 'int' }, { name: 'total_install', type: 'int' },
+                    { name: 'total_cr', type: 'int' }, { name: 'total_earning', type: 'int' },
+                    'description'
+                ],
+                idProperty: 'id',
+                proxy: {
+                    type: 'ajax',
+                    url: 'resources/data/topics.json',
+                    reader: {
+                        type: 'json',
+                        rootProperty: 'topics',
+                        totalProperty: 'totalCount'
+                    }
+                }
+            },
+            stateful: true,
+            viewModel: {
+                data: {
+                    expanded: true
+                }
+            },
+
+            plugins: [{
+                ptype: 'preview',
+
+                expanded: true,
+                bodyField: 'description'
+            }],
+
+            viewConfig: {
+                trackOver: false,
+                stripeRows: false
+            },
+
+            columns: {
+                items: [
+                    {
+                        text: "#",
+                        dataIndex: 'id',
+
+                        width: 50,
+                        align: 'right',
+                        sortable: true
+                    }, {
+                        text: "Name",
+                        dataIndex: 'name',
+
+                        flex: 1,
+                        sortable: false,
+                        renderer: 'renderName'
+                    }, {
+                        text: "OS",
+                        dataIndex: 'platform',
+
+                        width: 100,
+                        sortable: true
+                    }, {
+                        text: "Type",
+                        dataIndex: 'type',
+
+                        width: 70,
+                        align: 'right',
+                        sortable: true
+                    }, {
+                        text: "Region",
+                        dataIndex: 'region',
+
+                        width: 150
+                    }, {
+                        text: "Status",
+                        dataIndex: 'status',
+
+                        width: 150,
+                        sortable: true
+                    }, {
+                        text: "Total Click",
+                        dataIndex: 'total_click',
+
+                        width: 150
+                    }, {
+                        text: "Total Install",
+                        dataIndex: 'total_install',
+
+                        width: 150
+                    }, {
+                        text: "CR(%)",
+                        dataIndex: 'total_cr',
+
+                        width: 150
+                    }, {
+                        text: "Earning(VND)",
+                        dataIndex: 'total_earning',
+
+                        width: 150
+                    }, {
+                        menuDisabled: true,
+                        sortable: false,
+                        xtype: 'actioncolumn',
+                        width: 50,
+                        items: ['@detail']
+                    }
+                ]
+            },
+
+            bbar: {
+                xtype: 'pagingtoolbar',
+                displayInfo: true,
+                displayMsg: 'Displaying apps {0} - {1} of {2}',
+                emptyMsg: "No apps to display",
+
+                items: ['-', {
+                    bind: '{expanded ? "Hide Description" : "Show Description"}',
+                    pressed: '{expanded}',
+                    enableToggle: true,
+                    toggleHandler: 'onToggleExpanded'
+                }]
             }
         }
-    },
+    ]
 
-    viewModel: {
-        data: {
-            expanded: true
-        }
-    },
-
-    plugins: [{
-        ptype: 'preview',
-
-        expanded: true,
-        bodyField: 'description'
-    }],
-
-    viewConfig: {
-        trackOver: false,
-        stripeRows: false
-    },
-
-    columns: [{
-        text: "#",
-        dataIndex: 'id',
-
-        width: 50,
-        align: 'right',
-        sortable: true
-    }, {
-        text: "Name",
-        dataIndex: 'name',
-
-        flex: 1,
-        sortable: false,
-        renderer: 'renderName'
-    },{
-        text: "OS",
-        dataIndex: 'platform',
-
-        width: 100,
-        sortable: true
-    },{
-        text: "Type",
-        dataIndex: 'type',
-
-        width: 70,
-        align: 'right',
-        sortable: true
-    },{
-        text: "Region",
-        dataIndex: 'region',
-
-        width: 150
-    }, {
-        text: "Status",
-        dataIndex: 'status',
-
-        width: 150,
-        sortable: true
-    }, {
-        text: "Total Click",
-        dataIndex: 'total_click',
-
-        width: 150
-    }, {
-        text: "Total Install",
-        dataIndex: 'total_install',
-
-        width: 150
-    }, {
-        text: "CR(%)",
-        dataIndex: 'total_cr',
-
-        width: 150
-    }, {
-        text: "Earning(VND)",
-        dataIndex: 'total_earning',
-
-        width: 150
-    }, {
-        menuDisabled: true,
-        sortable: false,
-        xtype: 'actioncolumn',
-        width: 50,
-        items: ['@detail']
-    }],
-
-    bbar: {
-        xtype: 'pagingtoolbar',
-        displayInfo: true,
-        displayMsg: 'Displaying apps {0} - {1} of {2}',
-        emptyMsg: "No apps to display",
-
-        items: ['-', {
-            bind: '{expanded ? "Hide Description" : "Show Description"}',
-            pressed: '{expanded}',
-            enableToggle: true,
-            toggleHandler: 'onToggleExpanded'
-        }]
-    }
 });
